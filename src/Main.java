7a2868e130c2c90d98bd8e6a86f1b3b0215e86b9
enderlinp-uha <philippe.enderlin@uha.fr>
2025-02-03 12:04:17 +0100

    UPDATE src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         // Initialisation de la liste des produits
         ListeProduit produits = SListeProduit.getInstance();
         produits.creer(FProduit.creerProduit("Blade Runner", ECategorieProduit.LIVRE, 8.00, 5));
+        produits.creer(FProduit.creerProduit("2001 : L'Odyssée de l'espace", ECategorieProduit.LIVRE, 8.40, 3));
         produits.creer(FProduit.creerProduit("Interstellar", ECategorieProduit.DVD, 7.45, 10));
-        System.out.println(produits);
+
+        // Afficher la liste des produits
+        produits.afficher();
     }
 }
