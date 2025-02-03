5b9685729f9eea6996eb0e58c74c05272283e9bb
enderlinp-uha <philippe.enderlin@uha.fr>
2025-02-03 23:18:55 +0100

    ADD arrondirDouble function

--- src/Utils.java
+++ src/Utils.java
@@ -2,17 +2,21 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Utils {
-    public static String libelleMoyenPaiement(EMoyenPaiement type) {
-        return switch (type) {
-            case CREDIT -> "Carte bancaire";
-            case CRYPTO -> "Cryptomonnaie";
-            case PAYPAL -> "PayPal";
-        };
+    public static double arrondirDouble(double value) {
+        return Math.round(value * 100.0) / 100.0;
     }
 
     public static String formaterDate(Date date, String format) {
         SimpleDateFormat nouvelleDate = new SimpleDateFormat(format);
 
         return nouvelleDate.format(date);
+    }
+
+    public static String libelleMoyenPaiement(EMoyenPaiement type) {
+        return switch (type) {
+            case CREDIT -> "Carte bancaire";
+            case CRYPTO -> "Cryptomonnaie";
+            case PAYPAL -> "PayPal";
+        };
     }
 }
