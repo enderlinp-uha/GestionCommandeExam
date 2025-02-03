5f17a02f5f9e1c3db790a65d9a37489753b913c5
enderlinp-uha <philippe.enderlin@uha.fr>
2025-02-03 21:54:33 +0100

    UPDATE src/TransactionLogger.java

--- src/TransactionLogger.java
+++ src/TransactionLogger.java
@@ -9,7 +9,7 @@ public class TransactionLogger {
         this.log(ELogType.INFO, "Initialisation de l'enregistreur");
     }
 
-    private String prefixer(ELogType type, String message) {
+    private String prefixerLog(ELogType type, String message) {
         StringBuilder sb = new StringBuilder();
         sb.append(type);
         sb.append(" - ");
@@ -21,7 +21,7 @@ public class TransactionLogger {
     }
 
     public void log(ELogType type, String message) {
-        String entry = prefixer(type, message);
+        String entry = prefixerLog(type, message);
         logs.add(entry);
 
         System.out.println(entry);
