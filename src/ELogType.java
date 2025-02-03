dd6fcbd577ca65d7371c645ba49f49dc05c5ab07
enderlinp-uha <philippe.enderlin@uha.fr>
2025-02-03 11:40:18 +0100

    UPDATE TransactionLogger.java

---
+++ src/ELogType.java
@@ -0,0 +1,3 @@
+public enum ELogType {
+    INFO, WARNING, ERROR, FATAL
+}
--- src/TransactionLogger.java
+++ src/TransactionLogger.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class TransactionLogger {
+    private List<String> logs = new ArrayList<>();
+
+    public TransactionLogger() {
+        this.log(ELogType.INFO, "Logger initialisé");
+    }
+
+    private String prependLog(ELogType type, String message) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(type);
+        sb.append(" - ");
+        sb.append(Utils.formattedDate(new Date(), "yyyy-MM-dd HH:mm:ss"));
+        sb.append(" --> ");
+        sb.append(message);
+
+        return sb.toString();
+    }
+
+    public void log(ELogType type, String message) {
+        String entry = prependLog(type, message);
+        logs.add(entry);
+
+        System.out.println(entry);
+
+    }
 }
