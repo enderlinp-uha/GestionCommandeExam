999d8345d4fc7518dd1e7fda3015d61312b38c5e
enderlinp-uha <philippe.enderlin@uha.fr>
2025-02-03 21:55:53 +0100

    UPDATE src/SProduitBdd.java

--- src/SProduitBdd.java
+++ src/SProduitBdd.java
@@ -1,6 +1,8 @@
 public class SProduitBdd {
     private static ProduitBdd instance = null;
 
+    public SProduitBdd() {}
+
     public static ProduitBdd getInstance() {
         if (instance == null) {
             instance = new ProduitBdd();
