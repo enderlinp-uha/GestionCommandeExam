cbf0b11cb2959c26fe28e6c089f39562334a114c
enderlinp-uha <philippe.enderlin@uha.fr>
2025-02-03 21:44:20 +0100

    UPDATE src/FCommande.java

--- src/FCommande.java
+++ src/FCommande.java
@@ -9,4 +9,22 @@ public class FCommande {
                 .setMoyenPaiement(EMoyenPaiement.CREDIT)
                 .build();
     }
+
+    public static Commande creerCommandeCryptomonnaie(Client client, List<Produit> produits, double prixTotal) {
+        return new Commande.CommandeBuilder()
+                .setClient(client)
+                .setProduits(produits)
+                .setPrixTotal(prixTotal)
+                .setMoyenPaiement(EMoyenPaiement.CRYPTO)
+                .build();
+    }
+
+    public static Commande creerCommandePayPal(Client client, List<Produit> produits, double prixTotal) {
+        return new Commande.CommandeBuilder()
+                .setClient(client)
+                .setProduits(produits)
+                .setPrixTotal(prixTotal)
+                .setMoyenPaiement(EMoyenPaiement.PAYPAL)
+                .build();
+    }
 }
